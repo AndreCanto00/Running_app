53531b9ad3b9e396ec837a05dc8f1ccdd97b2c81
AndreCanto00 <112171091+AndreCanto00@users.noreply.github.com>
2025-02-19 08:25:00 +0000

    Fixing lint and format

--- Makefile
+++ Makefile
@@ -6,10 +6,10 @@ test:
 	python -m pytest -vv --cov=main --cov=calCLI --cov=mylib test_*.py
 
 format:	
-	black *.py mylib/*.py
+	black mylib/*.py
 
 lint:
-	pylint --disable=R,C --ignore-patterns=test_.*?py *.py  mylib/*.py
+	pylint --disable=R,C --ignore-patterns=test_.*?py  mylib/*.py
 
 container-lint:
 	docker run --rm -i hadolint/hadolint < Dockerfile
--- mylib/load.py
+++ mylib/load.py
@@ -1,24 +1,38 @@
-''' Let's build 3 function to calculate TRIMP, TRIMP_LT and HRRS. HRRS = TRIMP/TRIMP_LT. 
+"""Let's build 3 function to calculate TRIMP, TRIMP_LT and HRRS. HRRS = TRIMP/TRIMP_LT.
 TRIMP = =(((AVG_HR-REST_HR)/(MAX_HR-REST_HR))*0,64*EXP(((AVG_HR-REST_HR)/(MAX_HR-REST_HR))*1,92)*workout_duration)
 TRIMP_LT = =(((LT_HR-REST_HR)/(MAX_HR-REST_HR))*0,64*EXP(((LT_HR-REST_HR)/(MAX_HR-REST_HR))*1,92)*LT_duration) con LT_duration = workout_duration*0,5 = 60
-HRRS = TRIMP/TRIMP_LT*100'''
+HRRS = TRIMP/TRIMP_LT*100"""
 
 import math
 
+
 # Function trimp
 def trimp(avg_hr, max_hr, rest_hr, workout_duration):
-    trimp = (((avg_hr-rest_hr)/(max_hr-rest_hr))*0.64*math.exp(((avg_hr-rest_hr)/(max_hr-rest_hr))*1.92)*workout_duration)
-    return trimp
+    trimp_value = (
+        ((avg_hr - rest_hr) / (max_hr - rest_hr))
+        * 0.64
+        * math.exp(((avg_hr - rest_hr) / (max_hr - rest_hr)) * 1.92)
+        * workout_duration
+    )
+    return trimp_value
+
 
 # Function trimp_lt, workout_duration = 60
 def trimp_lt(lt_hr, max_hr, rest_hr, lt_duration=60):
-    trimp_lt = (((lt_hr-rest_hr)/(max_hr-rest_hr))*0.64*math.exp(((lt_hr-rest_hr)/(max_hr-rest_hr))*1.92)*lt_duration)
-    return trimp_lt
+    trimp_lt_value = (
+        ((lt_hr - rest_hr) / (max_hr - rest_hr))
+        * 0.64
+        * math.exp(((lt_hr - rest_hr) / (max_hr - rest_hr)) * 1.92)
+        * lt_duration
+    )
+    return trimp_lt_value
+
 
 # Function hrrs
 def hrrs(avg_hr, max_hr, rest_hr, workout_duration, lt_hr):
-    hrrs = trimp(avg_hr, max_hr, rest_hr, workout_duration)/trimp_lt(lt_hr, max_hr, rest_hr)*100
-    return hrrs
-
-
-
+    hrrs_value = (
+        trimp(avg_hr, max_hr, rest_hr, workout_duration)
+        / trimp_lt(lt_hr, max_hr, rest_hr)
+        * 100
+    )
+    return hrrs_value
